83b6c629c9252f1a132590bdd0668959a3c92813
spartha1 <leinadspartha@gmail.com>
2026-02-08 20:17:09 -0600

    feat: Add Docker Compose configuration and database initialization script; update database seeder and testing environment settings

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // Ejecutar seeders en orden
+        $this->call([
+            UserRoleExamplesSeeder::class,
         ]);
     }
 }
